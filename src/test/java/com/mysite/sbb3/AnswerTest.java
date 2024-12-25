d6572e851a3952422bb029730ca5548bc548f846
universechoi <skye.of.universe@gmail.com>
2024-12-25 22:59:55 +0900

    test: find answerList

--- src/test/java/com/mysite/sbb3/AnswerTest.java
+++ src/test/java/com/mysite/sbb3/AnswerTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -41,5 +43,17 @@ public class AnswerTest {
 
         Answer a = oa.get();
         assertEquals(2, a.getQuestion().getId());
+    }
+
+    @Transactional
+    @Test
+    void findAnswerListTest() {
+        Optional<Question> oq = this.questionRepository.findById(2L);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 }
