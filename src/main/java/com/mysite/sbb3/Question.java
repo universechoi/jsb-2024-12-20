506d8962aedf5e44be57cbc55f4330ff1f2301e1
universechoi <skye.of.universe@gmail.com>
2024-12-25 22:11:36 +0900

    feat: add Question entity

---
+++ src/main/java/com/mysite/sbb3/Question.java
@@ -0,0 +1,34 @@
+package com.mysite.sbb3;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Answer> answerList;
+}
