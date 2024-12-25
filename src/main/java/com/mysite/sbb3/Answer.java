39f6b947f63716172df201e46642a27df6085b65
universechoi <skye.of.universe@gmail.com>
2024-12-25 22:11:57 +0900

    feat: add Answer entity

---
+++ src/main/java/com/mysite/sbb3/Answer.java
@@ -0,0 +1,30 @@
+package com.mysite.sbb3;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
