285365d17d33559fea78d392081239e8913b5cec
universechoi <skye.of.universe@gmail.com>
2024-12-26 00:45:25 +0900

    refactor: add requestMapping

--- src/main/java/com/mysite/sbb3/question/QuestionController.java
+++ src/main/java/com/mysite/sbb3/question/QuestionController.java
@@ -5,23 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/question")
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
-    @GetMapping("/question/detail/{id}")
+    @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable("id") Long id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
