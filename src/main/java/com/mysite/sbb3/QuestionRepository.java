6daf9b2f5f0edd3810a9827ca2620c774dda3fbc
universechoi <skye.of.universe@gmail.com>
2024-12-25 22:26:09 +0900

    test: questionRepository.findBySubjectLike()

--- src/main/java/com/mysite/sbb3/QuestionRepository.java
+++ src/main/java/com/mysite/sbb3/QuestionRepository.java
@@ -2,8 +2,12 @@ package com.mysite.sbb3;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     Question findBySubject(String subject);
 
     Question findBySubjectAndContent(String subject, String content);
+
+    List<Question> findBySubjectLike(String subject);
 }
--- src/test/java/com/mysite/sbb3/Sbb3ApplicationTests.java
+++ src/test/java/com/mysite/sbb3/Sbb3ApplicationTests.java
@@ -61,4 +61,11 @@ class Sbb3ApplicationTests {
         Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
         assertEquals(1, q.getId());
     }
+
+    @Test
+    void findBySubjectLikeTest() {
+        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.getFirst();
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
 }
