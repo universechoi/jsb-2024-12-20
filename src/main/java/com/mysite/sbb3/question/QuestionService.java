aedc53b9a4b3b5be95458df5fa9af95d8db1228c
universechoi <skye.of.universe@gmail.com>
2024-12-26 00:44:09 +0900

    feat: add questionService.getQuestion()

--- src/main/java/com/mysite/sbb3/question/QuestionService.java
+++ src/main/java/com/mysite/sbb3/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.mysite.sbb3.question;
 
+import com.mysite.sbb3.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +15,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Long id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
