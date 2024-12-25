ab5a7603077956c18ab13542cd1f04bc667baf96
universechoi <skye.of.universe@gmail.com>
2024-12-25 21:56:25 +0900

    init

---
+++ src/main/java/com/mysite/sbb3/Sbb3Application.java
@@ -0,0 +1,11 @@
+package com.mysite.sbb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Sbb3Application.class, args);
+    }
+}
