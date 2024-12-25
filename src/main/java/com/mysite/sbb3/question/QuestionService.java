19e6ecd9972cf8ed024b1952f611d6a7fe22aeb1
universechoi <skye.of.universe@gmail.com>
2024-12-26 00:15:40 +0900

    feat: add questionService.getList()

--- src/main/java/com/mysite/sbb3/question/QuestionService.java
+++ src/main/java/com/mysite/sbb3/question/QuestionService.java
@@ -3,10 +3,15 @@ package com.mysite.sbb3.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
 }
