cc1febccd6dedf73bf901d71261bf9a868e2ca7d
universechoi <skye.of.universe@gmail.com>
2024-12-26 00:56:33 +0900

    feat: add AnswerController and create page

---
+++ src/main/java/com/mysite/sbb3/answer/AnswerController.java
@@ -0,0 +1,26 @@
+package com.mysite.sbb3.answer;
+
+import com.mysite.sbb3.question.Question;
+import com.mysite.sbb3.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+public class AnswerController {
+
+    private final QuestionService questionService;
+
+    @GetMapping("/create/{id}")
+    public String create(Model model, @PathVariable("id") Long id, @RequestParam(value = "content") String content) {
+        Question question = this.questionService.getQuestion(id);
+        // TODO : 답변 저장
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}
