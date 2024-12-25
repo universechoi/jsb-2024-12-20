f26dc20bdd9ae7350f4ee9c6b9ab6ffe5e36ead8
universechoi <skye.of.universe@gmail.com>
2024-12-26 01:32:54 +0900

    feat: add question form page

--- src/main/java/com/mysite/sbb3/question/QuestionController.java
+++ src/main/java/com/mysite/sbb3/question/QuestionController.java
@@ -29,4 +29,9 @@ public class QuestionController {
         model.addAttribute("question", question);
         return "question_detail";
     }
+
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
 }
