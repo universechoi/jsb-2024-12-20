cad10b5c6bfe30f8477477da18249158416d86b5
universechoi <skye.of.universe@gmail.com>
2024-12-25 22:24:23 +0900

    test: questionRepository.findBySubjectAndContent()

--- src/test/java/com/mysite/sbb3/Sbb3ApplicationTests.java
+++ src/test/java/com/mysite/sbb3/Sbb3ApplicationTests.java
@@ -55,4 +55,10 @@ class Sbb3ApplicationTests {
         Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
         assertEquals(1, q.getId());
     }
+
+    @Test
+    void findBySubjectAndContentTest() {
+        Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertEquals(1, q.getId());
+    }
 }
