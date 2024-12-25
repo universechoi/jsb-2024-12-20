7f7851200a479bed53799e60fb1f092f069ffe39
universechoi <skye.of.universe@gmail.com>
2024-12-25 22:41:37 +0900

    test: separate profile to test

--- src/test/java/com/mysite/sbb3/Sbb3ApplicationTests.java
+++ src/test/java/com/mysite/sbb3/Sbb3ApplicationTests.java
@@ -3,6 +3,7 @@ package com.mysite.sbb3;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class Sbb3ApplicationTests {
 
