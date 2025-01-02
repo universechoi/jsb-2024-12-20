68e98b75831277de3a647145d8262a3de3b5991e
universechoi <skye.of.universe@gmail.com>
2025-01-02 17:05:51 +0900

    feat: paging

--- src/main/java/com/mysite/sbb3/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb3/question/QuestionRepository.java
@@ -1,5 +1,7 @@
 package com.mysite.sbb3.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,6 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     Question findBySubjectAndContent(String subject, String content);
 
     List<Question> findBySubjectLike(String subject);
+
+    Page<Question> findAll(Pageable pageable);
 }
--- src/main/java/com/mysite/sbb3/question/QuestionService.java
+++ src/main/java/com/mysite/sbb3/question/QuestionService.java
@@ -2,9 +2,11 @@ package com.mysite.sbb3.question;
 
 import com.mysite.sbb3.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,8 +15,9 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return this.questionRepository.findAll(pageable);
     }
 
     public Question getQuestion(Long id) {
